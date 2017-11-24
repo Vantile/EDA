03d9b35a1094f3d0d206bb7d2f483bc620422a39
Vantile <vantilesyfon@gmail.com>
2017-11-24 23:29:02 +0100

    06 07 failed 08 09 10 OK 11 for testing 13 14 WIP

--- EDA2/11.cpp
+++ EDA2/11.cpp
@@ -7,10 +7,70 @@
 #include <fstream>
 #include <vector>
 
+enum piedrasPreciosas {diamante, rubi, esmeralda, zafiro, jade};
+
+std::istream& operator>>(std::istream& entrada, piedrasPreciosas& p){
+	char num;
+	entrada >> num;
+	switch(num){
+		case 'd': p = diamante; break;
+		case 'r': p = rubi; break;
+		case 'e': p = esmeralda; break;
+		case 'z': p = zafiro; break;
+		case 'j': p = jade; break;
+	}
+	return entrada;
+}
+
+int seqPiedras(const std::vector<piedrasPreciosas> &v, const int &seq, 
+	const piedrasPreciosas &p1, const int &p1num, const piedrasPreciosas &p2, const int &p2num){
+		int i = 0;
+		int numSeqOk = 0;
+		piedrasPreciosas primSeq = v[0];
+		int tamSeq = 0;
+		int p1numAct = 0;
+		int p2numAct = 0;
+		while(i < v.size()){
+			if(tamSeq < seq){
+				++tamSeq;
+				if(v[i] == p1) ++p1numAct;
+				if(v[i] == p2) ++p2numAct;
+			}
+			else{
+				if(p1numAct == p1num && p2numAct == p2num) ++numSeqOk;
+				if(primSeq == p1) --p1numAct;
+				if(primSeq == p2) --p2numAct;
+				if(v[i] == p1) ++p1numAct;
+				if(v[i] == p2) ++p2numAct;
+				primSeq = v[i - seq];
+			}
+			++i;
+		}
+		return numSeqOk;
+	}
+
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
-
+	int n, seq, p1num, p2num;
+	piedrasPreciosas p1, p2;
+	// Parámentros del problema
+	std::cin >> n;
+	std::cin >> seq;
+	std::cin >> p1;
+	std::cin >> p1num;
+	std::cin >> p2;
+	std::cin >> p2num;
+	// Vector
+	std::vector<piedrasPreciosas> v;
+	for(int i = 0; i < n; ++i){
+		piedrasPreciosas p;
+		std::cin >> p;
+		v.push_back(p);
+	}
+	int s = seqPiedras(v, seq, p1, p1num, p2, p2num);
+	std::cout << s << std::endl;
+	return true;
 }
 
 int main() {
